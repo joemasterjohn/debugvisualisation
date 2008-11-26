87873b9e1feeb3d79a973c02749faa4edd4d0cb1
balage.g <balage.g@ee7c0c50-ba3a-11dd-a828-c573283ffd12>
2008-11-26 10:58:13 +0000

    Extension point definiálása LayoutAlgorithm-ok becsatolására
    git-svn-id: https://debugvisualisation.googlecode.com/svn/trunk@14 ee7c0c50-ba3a-11dd-a828-c573283ffd12

---
+++ hu.gbalage.debugvisualisation/src/hu/gbalage/debugvisualisation/layouts/TreeLayoutCreator.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package hu.gbalage.debugvisualisation.layouts;
+
+import org.eclipse.zest.layouts.LayoutAlgorithm;
+import org.eclipse.zest.layouts.LayoutStyles;
+import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
+
+/**
+ * @author Grill Balazs (balage.g@gmail.com)
+ *
+ */
+public class TreeLayoutCreator implements ILayoutAlgorithmCreator {
+
+	/**
+	 * @see hu.gbalage.debugvisualisation.layouts.ILayoutAlgorithmCreator#create()
+	 */
+	public LayoutAlgorithm create() {
+		return new TreeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING);
+	}
+
+}
