1f7dda4f3630d151b9b533ac217ace963f8b3e5f
balage.g <balage.g@ee7c0c50-ba3a-11dd-a828-c573283ffd12>
2008-11-28 18:51:24 +0000

    version step :3.3
    git-svn-id: https://debugvisualisation.googlecode.com/svn/trunk@24 ee7c0c50-ba3a-11dd-a828-c573283ffd12

--- hu.gbalage.debugvisualisation/src/hu/gbalage/debugvisualisation/view/IDebugTreePresentation.java
+++ hu.gbalage.debugvisualisation/src/hu/gbalage/debugvisualisation/view/IDebugTreePresentation.java
@@ -7,19 +7,43 @@ import hu.gbalage.debugvisualisation.model.Edge;
 import hu.gbalage.debugvisualisation.model.Node;
 
 /**
+ * A presentation is supposed to display the graph described
+ * in the model. The model is notified about each changes on
+ * the model.
  * @author Grill Balazs (balage.g@gmail.com)
  *
  */
 public interface IDebugTreePresentation {
 
+	/**
+	 * A node is added to the model
+	 * @param node
+	 */
 	public void addNode(Node node);
 	
+	/**
+	 * A node is removed from the model
+	 * @param node
+	 */
 	public void removeNode(Node node);
 	
+	/**
+	 * An edge is added to the model
+	 * @param edge
+	 * @param from
+	 * @param to
+	 */
 	public void addEdge(Edge edge, Node from, Node to);
 	
+	/**
+	 * An edge is removed from the model
+	 * @param edge
+	 */
 	public void removeEdge(Edge edge);
 	
+	/**
+	 * The presentation should be refreshed
+	 */
 	public void refresh();
 	
 }
