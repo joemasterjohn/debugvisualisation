f57d4215f8e57cadc29f9ce1310f5259b0e47388
balage.g <balage.g@ee7c0c50-ba3a-11dd-a828-c573283ffd12>
2008-11-29 18:49:36 +0000

    DirectedGraphlayout changed to a DirectedGraphLayoutAlgorith+HorizontalShift
    git-svn-id: https://debugvisualisation.googlecode.com/svn/trunk@29 ee7c0c50-ba3a-11dd-a828-c573283ffd12

--- hu.gbalage.debugvisualisation/src/hu/gbalage/debugvisualisation/extensions/layouts/DirectedGraphLayoutCreator.java
+++ hu.gbalage.debugvisualisation/src/hu/gbalage/debugvisualisation/extensions/layouts/DirectedGraphLayoutCreator.java
@@ -5,7 +5,9 @@ package hu.gbalage.debugvisualisation.extensions.layouts;
 
 import org.eclipse.zest.layouts.LayoutAlgorithm;
 import org.eclipse.zest.layouts.LayoutStyles;
+import org.eclipse.zest.layouts.algorithms.CompositeLayoutAlgorithm;
 import org.eclipse.zest.layouts.algorithms.DirectedGraphLayoutAlgorithm;
+import org.eclipse.zest.layouts.algorithms.HorizontalShift;
 
 import hu.gbalage.debugvisualisation.layouts.ILayoutAlgorithmCreator;
 
@@ -19,7 +21,12 @@ public class DirectedGraphLayoutCreator implements ILayoutAlgorithmCreator {
 	 * @see hu.gbalage.debugvisualisation.layouts.ILayoutAlgorithmCreator#create()
 	 */
 	public LayoutAlgorithm create() {
-		return new DirectedGraphLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING);
+		//return new DirectedGraphLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING);
+		return new
+		CompositeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING, new
+				LayoutAlgorithm[] { new
+				DirectedGraphLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), new
+				HorizontalShift(LayoutStyles.NO_LAYOUT_NODE_RESIZING) });
 	}
 
 }
