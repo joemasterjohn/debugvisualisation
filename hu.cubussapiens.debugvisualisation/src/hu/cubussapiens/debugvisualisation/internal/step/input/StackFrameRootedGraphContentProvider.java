f35bf4e39b11ce868de8470c7394c719822ae7ef
balage.g@gmail.com <balage.g@gmail.com@ee7c0c50-ba3a-11dd-a828-c573283ffd12>
2009-07-31 12:54:32 +0000

    Root item of the transformation chain
    git-svn-id: https://debugvisualisation.googlecode.com/svn/trunk@153 ee7c0c50-ba3a-11dd-a828-c573283ffd12

---
+++ hu.cubussapiens.debugvisualisation/src/hu/cubussapiens/debugvisualisation/internal/step/input/StackFrameRootedGraphContentProvider.java
@@ -0,0 +1,106 @@
+/**
+ * 
+ */
+package hu.cubussapiens.debugvisualisation.internal.step.input;
+
+import hu.cubussapiens.debugvisualisation.Activator;
+import hu.cubussapiens.debugvisualisation.internal.step.IRootedGraphContentProvider;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.model.IStackFrame;
+import org.eclipse.debug.core.model.IValue;
+import org.eclipse.debug.core.model.IVariable;
+
+/**
+ * This class provides the graph content based on a stack frame
+ */
+public class StackFrameRootedGraphContentProvider implements
+		IRootedGraphContentProvider {
+
+	/**
+	 * 
+	 */
+	public final Object root = new Integer(-1);
+
+	final IStackFrame sf;
+
+	/**
+	 * @param sf
+	 * 
+	 */
+	public StackFrameRootedGraphContentProvider(IStackFrame sf) {
+		this.sf = sf;
+	}
+
+	/* (non-Javadoc)
+	 * @see hu.cubussapiens.debugvisualisation.internal.step.IRootedGraphContentProvider#getChilds(java.lang.Object)
+	 */
+	public Collection<Object> getChilds(Object node) {
+		List<Object> os = new ArrayList<Object>();
+		if (root.equals(node)) {
+			try {
+				for (IVariable v : sf.getVariables()) {
+					os.add(v.getValue());
+				}
+			} catch (DebugException e) {
+				Activator.getDefault().logError(e,
+						"Can't list context variables");
+			}
+		}
+		if (node instanceof IValue) {
+			IValue value = (IValue) node;
+			try {
+				for (IVariable v : value.getVariables()) {
+					os.add(v.getValue());
+				}
+			} catch (DebugException e) {
+				Activator.getDefault().logError(e, "Can't list variables");
+			}
+		}
+		return os;
+	}
+
+	/* (non-Javadoc)
+	 * @see hu.cubussapiens.debugvisualisation.internal.step.IRootedGraphContentProvider#getEdge(java.lang.Object, java.lang.Object)
+	 */
+	public Collection<Object> getEdge(Object nodea, Object nodeb) {
+		List<Object> os = new ArrayList<Object>();
+		if (root.equals(nodea)) {
+			try {
+				for (IVariable v : sf.getVariables()) {
+					if (v.getValue().equals(nodeb))
+						os.add(v);
+				}
+			} catch (DebugException e) {
+				Activator.getDefault().logError(e,
+						"Can't list context variables");
+			}
+		}
+		if (nodea instanceof IValue) {
+			IValue value = (IValue) nodea;
+			try {
+				for (IVariable v : value.getVariables()) {
+					if (v.getValue().equals(nodeb))
+						os.add(v);
+				}
+			} catch (DebugException e) {
+				Activator.getDefault().logError(e, "Can't list variables");
+			}
+		}
+		return os;
+	}
+
+	/* (non-Javadoc)
+	 * @see hu.cubussapiens.debugvisualisation.internal.step.IRootedGraphContentProvider#getRoots()
+	 */
+	public Collection<Object> getRoots() {
+		List<Object> roots = new ArrayList<Object>();
+		roots.add(root);
+		return roots;
+	}
+
+}
