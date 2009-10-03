99a053f7aefac3caf3a8c65935aeec9194948084
stampie.hun@gmail.com <stampie.hun@gmail.com@ee7c0c50-ba3a-11dd-a828-c573283ffd12>
2009-10-03 09:30:39 +0000

    Moved test project to trunk
    git-svn-id: https://debugvisualisation.googlecode.com/svn/trunk@205 ee7c0c50-ba3a-11dd-a828-c573283ffd12

---
+++ hu.cubussapiens.debugvisualisation.tests/src/debugproba/Valami.java
@@ -0,0 +1,24 @@
+package debugproba;
+
+import java.util.HashSet;
+
+
+public class Valami {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		HashSet<Object> set = new HashSet<Object>();
+		
+		set.add(2);
+		set.add(2.3);
+		set.add(2.4f);
+		set.add("foo");
+		
+		System.out.println();
+	}
+
+}
