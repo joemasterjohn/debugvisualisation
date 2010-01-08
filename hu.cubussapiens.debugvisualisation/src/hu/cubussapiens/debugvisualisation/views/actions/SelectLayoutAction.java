171590d005d71e853b5c2f7a28d761fd1f525c2a
Stampie.hun <Stampie.hun@ee7c0c50-ba3a-11dd-a828-c573283ffd12>
2010-01-08 22:51:09 +0000

    Incomplete - issue 55: Ordering of child nodes","Type-Enhancement, Priority-Medium http://code.google.com/p/debugvisualisation/issues/detail?id=55 Test implementation created
    git-svn-id: https://debugvisualisation.googlecode.com/svn/trunk@243 ee7c0c50-ba3a-11dd-a828-c573283ffd12

--- hu.cubussapiens.debugvisualisation/src/hu/cubussapiens/debugvisualisation/views/actions/SelectLayoutAction.java
+++ hu.cubussapiens.debugvisualisation/src/hu/cubussapiens/debugvisualisation/views/actions/SelectLayoutAction.java
@@ -1,8 +1,16 @@
 package hu.cubussapiens.debugvisualisation.views.actions;
 
+import hu.cubussapiens.debugvisualisation.internal.model.IDVValue;
 import hu.cubussapiens.zestlayouts.LayoutRegistry.LayoutEntry;
+
+import java.util.Comparator;
+
+import org.eclipse.debug.core.DebugException;
 import org.eclipse.jface.action.Action;
 import org.eclipse.zest.core.viewers.GraphViewer;
+import org.eclipse.zest.core.widgets.GraphNode;
+import org.eclipse.zest.layouts.LayoutAlgorithm;
+import org.eclipse.zest.layouts.LayoutEntity;
 
 /**
  * An action for selecting a layout
@@ -40,10 +48,38 @@ public class SelectLayoutAction extends Action {
 	 */
 	@Override
 	public void run() {
-		if (viewer != null)
-			viewer.setLayoutAlgorithm(layout.getLayoutCreator().create(), true);
+		if (viewer != null) {
+			LayoutAlgorithm algorithm = layout.getLayoutCreator().create();
+			algorithm.setComparator(new ValueComparator());
+			viewer.setLayoutAlgorithm(algorithm, true);
+		}
 		group.selectLayout(this);
 		super.run();
+
+	}
+
+	private class ValueComparator implements Comparator {
+
+		public int compare(Object o1, Object o2) {
+			Object element1 = ((GraphNode) ((LayoutEntity) o1).getGraphData())
+					.getData();
+			Object element2 = ((GraphNode) ((LayoutEntity) o2).getGraphData())
+					.getData();
+			if (element1 instanceof IDVValue && element2 instanceof IDVValue) {
+				IDVValue value1 = (IDVValue) element1, value2 = (IDVValue) element2;
+				try {
+					String name1 = value1.getParent().getRelatedVariable()
+							.getName();
+					String name2 = value2.getParent().getRelatedVariable()
+							.getName();
+					return name1.compareTo(name2);
+				} catch (DebugException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return 0;
+		}
 
 	}
 
