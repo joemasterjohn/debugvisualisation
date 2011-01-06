0fd0b07b4bdc9e6c227f2d2050104d72bc73096c
stampie.hun <stampie.hun@ee7c0c50-ba3a-11dd-a828-c573283ffd12>
2011-01-06 22:09:39 +0000

    Setting parameters for value hover to prove not infinitely big
    git-svn-id: https://debugvisualisation.googlecode.com/svn/trunk@314 ee7c0c50-ba3a-11dd-a828-c573283ffd12

--- hu.cubussapiens.debugvisualisation/src/hu/cubussapiens/debugvisualisation/internal/ValueHover.java
+++ hu.cubussapiens.debugvisualisation/src/hu/cubussapiens/debugvisualisation/internal/ValueHover.java
@@ -9,10 +9,13 @@ import org.eclipse.debug.core.model.IValue;
 import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.debug.ui.IDebugModelPresentation;
 import org.eclipse.debug.ui.IValueDetailListener;
+import org.eclipse.draw2d.FlowLayout;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.MarginBorder;
 import org.eclipse.draw2d.Panel;
-import org.eclipse.draw2d.ToolbarLayout;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.text.FlowPage;
+import org.eclipse.draw2d.text.TextFlow;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
@@ -37,22 +40,30 @@ public class ValueHover extends Panel {
 	public ValueHover(IDVValue value) {
 		super();
 
-		setLayoutManager(new ToolbarLayout(false));
+		setLayoutManager(new FlowLayout(false));
 		setBorder(new MarginBorder(2));
+		setMaximumSize(new Dimension(500, 100));
+		setPreferredSize(300, 100);
 
 		Label caption = new Label("Value:");
 		caption.setFont(CAPTION_FONT);
 		add(caption);
 
-		final Label valueLabel = new Label("computing...");
+		final FlowPage page = new FlowPage();
+		page.setMaximumSize(new Dimension(500, 150));
+		page.setPreferredSize(300, -1);
+		add(page);
+		final TextFlow valueLabel = new TextFlow("computing...");
 		valueLabel.setFont(VALUE_FONT);
-		add(valueLabel);
+
+		page.add(valueLabel);
 
 		modelPresentation = DebugUITools.newDebugModelPresentation();
 		modelPresentation.computeDetail(value.getRelatedValue(),
 				new IValueDetailListener() {
 					public void detailComputed(IValue value, String result) {
 						valueLabel.setText(result);
+						valueLabel.setPreferredSize(300, -1);
 					}
 				});
 	}
