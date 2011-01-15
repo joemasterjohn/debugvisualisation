58593c2bc940b92b1a3edc12680663cae4182de1
stampie.hun <stampie.hun@ee7c0c50-ba3a-11dd-a828-c573283ffd12>
2011-01-15 13:51:04 +0000

    Truncating too long value hovers
    git-svn-id: https://debugvisualisation.googlecode.com/svn/trunk@317 ee7c0c50-ba3a-11dd-a828-c573283ffd12

--- hu.cubussapiens.debugvisualisation/src/hu/cubussapiens/debugvisualisation/internal/ValueHover.java
+++ hu.cubussapiens.debugvisualisation/src/hu/cubussapiens/debugvisualisation/internal/ValueHover.java
@@ -10,10 +10,10 @@ import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.debug.ui.IDebugModelPresentation;
 import org.eclipse.debug.ui.IValueDetailListener;
 import org.eclipse.draw2d.FlowLayout;
-import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.MarginBorder;
 import org.eclipse.draw2d.Panel;
 import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.text.BlockFlow;
 import org.eclipse.draw2d.text.FlowPage;
 import org.eclipse.draw2d.text.TextFlow;
 import org.eclipse.swt.SWT;
@@ -35,6 +35,10 @@ public class ValueHover extends Panel {
 	private static final Font VALUE_FONT = new Font(Display.getDefault(),
 			FONT_DATA);
 
+	FlowPage page;
+	BlockFlow valueBlock;
+	TextFlow valueText;
+
 	private IDebugModelPresentation modelPresentation;
 
 	public ValueHover(IDVValue value) {
@@ -45,25 +49,52 @@ public class ValueHover extends Panel {
 		setMaximumSize(new Dimension(500, 100));
 		setPreferredSize(300, 100);
 
-		Label caption = new Label("Value:");
-		caption.setFont(CAPTION_FONT);
-		add(caption);
+		// Label caption = new Label("Value:");
+		// caption.setFont(CAPTION_FONT);
+		// add(caption);
 
-		final FlowPage page = new FlowPage();
+		page = new FlowPage();
 		page.setMaximumSize(new Dimension(500, 150));
 		page.setPreferredSize(300, -1);
+		page.setOpaque(true);
 		add(page);
-		final TextFlow valueLabel = new TextFlow("computing...");
-		valueLabel.setFont(VALUE_FONT);
 
-		page.add(valueLabel);
+		BlockFlow captionBlock = new BlockFlow();
+		page.add(captionBlock);
+		TextFlow contents = new TextFlow("Value");
+		contents.setFont(CAPTION_FONT);
+		captionBlock.add(contents);
+
+		valueBlock = new BlockFlow();
+		// valueBlock.setLayoutManager(new BlockFlowLayout(valueBlock));
+		page.add(valueBlock);
+		// valueLabel = new TextFlow("computing...");
+		// valueLabel.setFont(VALUE_FONT);
+		//
+		// page.add(valueLabel);
+
+		valueText = new TextFlow("Computing...");
+		// valueText.set
+		valueBlock.add(valueText);
+		// valueText.setLayoutManager(new ParagraphTextLayout(valueText,
+		// ParagraphTextLayout.WORD_WRAP_TRUNCATE));
+		valueBlock.setMaximumSize(new Dimension(300, -1));
+		// valueBlock.invalidate();
 
 		modelPresentation = DebugUITools.newDebugModelPresentation();
 		modelPresentation.computeDetail(value.getRelatedValue(),
 				new IValueDetailListener() {
 					public void detailComputed(IValue value, String result) {
-						valueLabel.setText(result);
-						valueLabel.setPreferredSize(300, -1);
+						// if (valueText != null) {
+						// valueBlock.remove(valueText);
+						// }
+						valueText.setText(result);
+						// valueText.setLayoutManager(new
+						// ParagraphTextLayout(valueText,
+						// ParagraphTextLayout.WORD_WRAP_TRUNCATE));
+						// valueText.invalidate();
+						// valueLabel.setText(result);
+
 					}
 				});
 	}
